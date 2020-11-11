a32d05328053dfe75213eacfdd9dbedb475eb8d9
“Enter <rahulrajan5410@gmail.com>
2020-11-11 17:17:50 +0530

    added partime

--- Employee.java
+++ Employee.java
@@ -37,3 +37,33 @@ class fullTimeEmployees extends Employee
         System.out.println("Enter Salary:");
         salary = get.nextFloat();
     }
+ void display()
+    {
+        System.out.println("=============================="+"\n"+"Full Time Employee Details"+"\n"+"=============================="+"\n");
+        super.display();
+        System.out.println("Salary: "+salary);
+        System.out.println("Designation: "+des);
+    }
+}
+ 
+class partTimeEmployees extends Employee
+{
+    int workinghrs, rate;
+    partTimeEmployees()
+    {
+        System.out.println("Enter Number of Working Hours:");
+        workinghrs = get.nextInt();
+    }
+    void calculatepay()
+    {
+        rate = 8 * workinghrs;
+    }
+ 
+    void display()
+    {
+        System.out.println("=============================="+"\n"+"Part Time Employee Details"+"\n"+"=============================="+"\n");
+        super.display();
+        System.out.println("Number of Working Hours: "+workinghrs);
+        System.out.println("Salary for "+workinghrs+" working hours is: $"+rate);
+    }
+}
